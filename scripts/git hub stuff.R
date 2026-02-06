23c1b8a9b63ee384dd4247b733b870a25e299419
olivia jezior <sst24msu@uea.ac.uk>
2026-02-06 14:01:42 +0000

    Add cleaning steps to raw data

--- scripts/git hub stuff.R
+++ scripts/git hub stuff.R
@@ -5,3 +5,10 @@ usethis::use_git_config(
 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
 
 gitcreds::gitcreds_set()
+
+
+usethis::use_git_config(
+    user.name  = "olivia jezior",        
+    user.email = "sst24msu@uea.ac.uk" 
+   )
+gitcreds::gitcreds_set()
