f54c6dd5c30e8270903a7029f01be5671a1a2937
liv4994 <sst24msu@uea.ac.uk>
2026-02-06 14:26:19 +0000

    Update git hub stuff.R

--- scripts/git hub stuff.R
+++ scripts/git hub stuff.R
@@ -1,14 +1,1 @@
-usethis::use_git_config(
-  user.name  = "olivia jezior",        
-  user.email = "sst24msu@uea.ac.uk" 
-)
-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
 
-gitcreds::gitcreds_set()
-
-
-usethis::use_git_config(
-    user.name  = "olivia jezior",        
-    user.email = "sst24msu@uea.ac.uk" 
-   )
-gitcreds::gitcreds_set()
